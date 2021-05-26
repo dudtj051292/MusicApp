02c5fd769ece583e2dacaf1a98e8c92617f46bae
KimYoungsuh <dudtj0512@naver.com>
2021-05-27 00:22:41 +0900

    MusicApp 완성

--- MusicApp/Player/PlayerViewController.swift
+++ MusicApp/Player/PlayerViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import AVFoundation
+import Foundation
 
 class PlayerViewController: UIViewController {
 
@@ -17,21 +19,101 @@ class PlayerViewController: UIViewController {
     @IBOutlet weak var totalDurationLabel: UILabel!
     @IBOutlet weak var playControlButton: UIButton!
     
-    let simplepPlayer = SimplePlayer.shared
+    let simplePlayer = SimplePlayer.shared
     
-    var timeObserver : Any?
-    var isSeeking : Bool = false
-    
-    
+    var timeObserver: Any?
+    var isSeeking: Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updatePlayButton()
+        updateTime(time: CMTime.zero)
+        timeObserver = simplePlayer.addPerioidicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 10), queue: DispatchQueue.main) { time in
+            self.updateTime(time: time)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+        updateTintColor()
+        updateTrackInfo()
     }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        simplePlayer.pause()
+        simplePlayer.replaceCurrentItem(with: nil)
+    }
+    
+    @IBAction func beginDrag(_ sender: UISlider) {
+        isSeeking = true
+    }
+    
+    @IBAction func endDrag(_ sender: UISlider) {
+        isSeeking = false
+    }
+    
+    @IBAction func seek(_ sender: UISlider) {
+        guard let currentItem = simplePlayer.currentItem else { return }
+        let position = Double(sender.value)
+        let seconds = currentItem.duration.seconds * position
+        let time = CMTime(seconds: seconds, preferredTimescale: 100)
+        simplePlayer.seek(to: time)
+    }
+    
+    @IBAction func togglePlayButton(_ sender: UIButton) {
+        if simplePlayer.isPlaying {
+            simplePlayer.pause()
+        } else {
+            simplePlayer.play()
+        }
+        updatePlayButton()
+    }
+}
 
+extension PlayerViewController {
+    func updateTrackInfo() {
+        guard let track = simplePlayer.currentItem?.convertToTrack() else { return }
+        thumbnailImageView.image = track.artwork
+        titleLabel.text = track.title
+        artistLabel.text = track.artist
+    }
+    
+    func updateTintColor() {
+        playControlButton.tintColor = DefaultStyle.Colors.tint
+        timeSlider.tintColor = DefaultStyle.Colors.tint
+    }
+    
+    func updateTime(time: CMTime) {
+        // print(time.seconds)
+        // currentTime label, totalduration label, slider
+        currentTimeLabel.text = secondsToString(sec: simplePlayer.currentTime)   // 3.1234 >> 00:03
+        totalDurationLabel.text = secondsToString(sec: simplePlayer.totalDuration)  // 39.2045  >> 00:39
+        
+        if isSeeking == false {
+            // 노래 들으면서 시킹하면, 자꾸 슬라이더가 업데이트 됨, 따라서 시킹아닐때마 슬라이더 업데이트하자
+            timeSlider.value = Float(simplePlayer.currentTime/simplePlayer.totalDuration)
+        }
+    }
+    
+    func secondsToString(sec: Double) -> String {
+        guard sec.isNaN == false else { return "00:00" }
+        let totalSeconds = Int(sec)
+        let min = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d", min, seconds)
+    }
+    
+    func updatePlayButton() {
+        if simplePlayer.isPlaying {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "pause.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        } else {
+            let configuration = UIImage.SymbolConfiguration(pointSize: 40)
+            let image = UIImage(systemName: "play.fill", withConfiguration: configuration)
+            playControlButton.setImage(image, for: .normal)
+        }
+    }
 }
